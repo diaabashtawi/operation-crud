829b45a3d98e48f13cb91876a3f251fec768013e
diaabashtawi <diaabashtawi@gmail.com>
2023-03-02 22:14:56 +0400

    add json ignore properties to solve serialization process

--- src/main/java/com/crud/operation/controller/VehicleController.java
+++ src/main/java/com/crud/operation/controller/VehicleController.java
@@ -1,4 +1,25 @@
 package com.crud.operation.controller;
 
+import com.crud.operation.entity.Vehicle;
+import com.crud.operation.repository.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class VehicleController {
+
+    private VehicleRepository vehicleRepository;
+
+    @Autowired
+    public VehicleController(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    @RequestMapping(
+            value = "/vehicles"
+    )
+    public Iterable<Vehicle> getAllVehicle(){
+        return vehicleRepository.findAll();
+    }
 }
--- src/main/java/com/crud/operation/entity/Owner.java
+++ src/main/java/com/crud/operation/entity/Owner.java
@@ -1,9 +1,15 @@
 package com.crud.operation.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@JsonIgnoreProperties(
+        {"hibernateLazyInitializer", "handler"}
+)
 public class Owner {
 
     @Id
@@ -14,6 +20,7 @@ public class Owner {
 
     private String firstName, lastName;
 
+    @JsonIgnore
     @OneToMany(
             cascade = CascadeType.ALL,
             mappedBy = "owner"
