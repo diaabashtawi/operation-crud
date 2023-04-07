08da9b9cd333e6b6d45095e4c4dd66a880021804
Diaa Bashtawi <diaabashtawi@gmail.com>
2023-04-08 00:13:44 +0400

    update the SecurityConfig Class

--- src/main/java/com/crud/operation/SecurityConfig.java
+++ src/main/java/com/crud/operation/SecurityConfig.java
@@ -3,9 +3,14 @@ package com.crud.operation;
 import com.crud.operation.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configurable
@@ -21,5 +26,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(new BCryptPasswordEncoder());
     }
 
+    @Bean
+    public AuthenticationManager getAuthenticationManager() throws Exception{
+        return authenticationManager();
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                // POST request to /login endpoint is not secured
+                .antMatchers(HttpMethod.POST, "/login")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
 }
